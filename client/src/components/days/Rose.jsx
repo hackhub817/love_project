f560691dbec3996969646ad345c8e9fed0962454
hackhub817 <piyushguptaji123@gmail.com>
2025-01-26 12:19:38 +0530

    deploy

--- client/src/components/days/Rose.jsx
+++ client/src/components/days/Rose.jsx
@@ -1,1 +1,27 @@
-export const Rose = () => {};
+import bg from "../../assets/Rose/rosebg.png";
+import dog1 from "../../assets/Rose/dog.png";
+import dog2 from "../../assets/Rose/dog2.png";
+import dog3 from "../../assets/Rose/dog3.png";
+import side from "../../assets/Rose/side.png";
+import sideview from "../../assets/Rose/sideview.png";
+import tree from "../../assets/Rose/tree.png";
+import propose from "../../assets/Rose/propose.png";
+import surprise from "../../assets/Rose/surprise.png";
+
+export const Rose = () => {
+  return (
+    <>
+      <div className="bg-pink-200 h-screen py-2 relative">
+        {/* Background Image with Opacity */}
+        <div
+          className="absolute inset-0 bg-cover bg-center"
+          style={{
+            backgroundImage: `url(${bg})`,
+          }}
+        >
+          <div className="absolute inset-0 bg-white opacity-[0.5]"></div>
+        </div>
+      </div>
+    </>
+  );
+};
