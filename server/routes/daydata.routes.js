267a293859e80774bce5ab54c03c0da0b810adea
hackhub817 <piyushguptaji123@gmail.com>
2025-01-30 20:17:25 +0530

    file size

--- server/routes/daydata.routes.js
+++ server/routes/daydata.routes.js
@@ -21,7 +21,7 @@ const router = Router();
 const upload = multer({
   dest: "uploads/",
   limits: {
-    fileSize: 500 * 1024 * 1024,
+    fileSize: 1000 * 1024 * 1024,
   },
   fileFilter: (req, file, cb) => {
     if (file.mimetype.startsWith("image/")) {
